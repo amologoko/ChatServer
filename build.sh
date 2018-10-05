a4b63ef6b1ff5b6730c8e90a261ab3b74b349945
Sasha M <sasham@gmail.com>
2018-10-05 16:49:03 -0400

    Uncommented conan installation in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#pip install conan
-#conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
-#conan remote add vthiery https://api.bintray.com/conan/vthiery/conan-packages
+pip install conan
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+conan remote add vthiery https://api.bintray.com/conan/vthiery/conan-packages
 conan install -s build_type=Debug .
 conan build .
